d05e80e251a5cd9fc3c2458a6ae6e029bfd8889c
cristinaj1 <cristinacee305@gmail.com>
2022-02-18 22:12:26 +0100

    Ejercicio ejercito hecho y completado

---
+++ src/main/java/tarea5d/EjercitoOrdenado.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea5d;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author Cris
+ */
+public class EjercitoOrdenado {
+
+    private Set<Soldado> lista;
+
+    //Constructor
+    public EjercitoOrdenado() {
+        this.lista = new TreeSet<>();
+    }
+
+    //Saber el número de soldados
+    public int numeroSoldados() {
+        return lista.size();
+    }
+
+    //Método el cual alista a los soldados(los añade)
+    public void alistarSoldado(Soldado soldado) {
+        lista.add(soldado);
+    }
+
+    //Método el cual sabe si el ejercito no tiene soldados
+    public boolean noSoldados() {
+        return lista.isEmpty();
+
+    }
+
+    //Método el cual dice si hay un soldado alistado
+    public boolean containsSoldado(Soldado s) {
+
+        return lista.contains(s);
+    }
+
+    //Método el cual saca todos los soldados en forma de ArrayList
+    public ArrayList<Soldado> arraySoldados() {
+        ArrayList<Soldado> listaSoldados = new ArrayList<>();
+        for (Soldado soldado : lista) {
+            listaSoldados.add(soldado);
+        }
+        return listaSoldados;
+    }
+
+    //Método el cual desmatricula a un soldado del ejército.
+    public void desmatricularSoldado(Soldado s) {
+        lista.remove(s);
+
+    }
+
+}
---
+++ src/main/java/tarea5d/PruebaEjercitoOrdenado.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea5d;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Cris
+ */
+public class PruebaEjercitoOrdenado {
+
+    public static void main(String[] args) {
+        //Creamos la lista de soldados
+        EjercitoOrdenado lista = new EjercitoOrdenado();
+        //ArrayList de soldados
+        ArrayList<Soldado> listaSoldados = new ArrayList<>();
+
+        //Añadimos objeros a la lista
+        lista.alistarSoldado(new Soldado("09117894L", "Pepe", "Martinez", "Velez", 23));
+        lista.alistarSoldado(new Soldado("09117876N", "Antonio", "Fernández", "Martinez", 40));
+        lista.alistarSoldado(new Soldado("01117898T", "Manuela", "Jiménez", "Bénitez", 20));
+        lista.alistarSoldado(new Soldado("22117894L", "Antonia", "Vazquez", "Vera", 18));
+        lista.alistarSoldado(new Soldado("09117897P", "Juan", "Sanchez", "Chacon", 30));
+
+        System.out.println("En el ejercito hay: " + lista.numeroSoldados() + " soldados");
+        listaSoldados = lista.arraySoldados();
+        for (Soldado listaSoldado : listaSoldados) {
+            System.out.println(listaSoldado);
+        }
+        lista.containsSoldado(new Soldado("09117894L", "Pepe", "Martinez", "Velez", 23));
+        lista.desmatricularSoldado(new Soldado("09117897P", "Juan", "Sanchez", "Chacon", 30));
+        System.out.println("¿La lista de soldados está vacía? " + lista.noSoldados());
+        boolean resultado = lista.containsSoldado(new Soldado("22117894Q", "Antonia", "Vazquez", "Vera", 18));
+        System.out.println("¿Está el soldado introducido?: " + resultado);
+    }
+
+}
